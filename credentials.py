becacb48a5ffbd84ecc0d5983283b51cc81eea47
Finn Dargavel <finndargavel@gmail.com>
2025-02-23 00:18:57 +1100

    'abc'

--- credentials.py
+++ credentials.py
@@ -1,5 +1,11 @@
-DBNAME = "austender" 
-USER = 'postgres'
-PASSWORD = 'password'
-HOST = 'localhost'
-PORT = '5432'
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
